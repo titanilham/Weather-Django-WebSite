02eec997e1eebab4af8cbc3984b27dd97a6ba4ff
Ilham <86422270+titanilham@users.noreply.github.com>
2024-10-11 01:08:23 +0300

    update

--- weather/WeatherApp/urls.py
+++ weather/WeatherApp/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),  # Убедитесь, что здесь только index
+    path('', views.index, name='index'),
     path('settings', views.settings, name="settings"),
-    path('login', views.login, name="login")
+    path('login', views.login, name="login"),
 ]
--- weather/WeatherApp/views.py
+++ weather/WeatherApp/views.py
@@ -1,4 +1,3 @@
-
 import requests
 from django.shortcuts import render
 from .config import API_KEY
@@ -10,26 +9,28 @@ def index(request):
     error = ""
     city_name = ""
 
-    if request.method == "POST":
-        if city := request.POST.get("city"):
-            try:
-                response = requests.get(
-                    f"http://api.weatherapi.com/v1/current.json?key={API_KEY}&q={city}&aqi=no",
-                    timeout=10
-                )
-                print(response.text)
-                response.raise_for_status()
-                data = response.json()
-                weather = data["current"]["temp_c"]
-                weather = round(float(weather))
-                city_name = data["location"]["name"]
-            except requests.RequestException as e:
-                error = "Check city name"
-            except KeyError:
-                error = "Incorrect weather service response."
+    # Получаем город из GET-параметров, если метод GET
+    city = request.GET.get("city") if request.method == "GET" else request.POST.get("city")
 
-        else:
-            error = "No city specified."
+    if city:
+        try:
+            response = requests.get(
+                f"http://api.weatherapi.com/v1/current.json?key={API_KEY}&q={city}&aqi=no",
+                timeout=10
+            )
+            print(response.text)
+            response.raise_for_status()
+            data = response.json()
+            weather = data["current"]["temp_c"]
+            weather = round(float(weather))
+            city_name = data["location"]["name"]
+        except requests.RequestException as e:
+            error = "Check city name"
+        except KeyError:
+            error = "Incorrect weather service response."
+    else:
+        error = "No city specified."
+    
     return render(request, "index.html", {"weather": weather, "error": error, "city_name": city_name})
 
 def settings(request):
